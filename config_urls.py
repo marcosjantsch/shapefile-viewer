0a5b2555db3c6914e8481e19d1fcb935b29887de
marcosjantsch <85318367+marcosjantsch@users.noreply.github.com>
2026-03-02 08:52:13 -0300

    Update config_urls.py

--- config_urls.py
+++ config_urls.py
@@ -15,13 +15,13 @@ CSV_URLS = {
     2012: "",
     2013: "https://1drv.ms/x/c/8b88b81c064543d3/IQCoP83W-FYER5KQu8DBuTTaAWacNQ23ZuxXLpHH_-NMqZ4?e=rdeZd5",
     2014: "",
-    2015: "",
+    2015: "https://1drv.ms/x/c/8b88b81c064543d3/IQBomFaAecYAQL4Nsbj0Rzr0AaWsk2ixz4heNKF9Lu3r8ug?e=Xy7VGU",
     2016: "https://1drv.ms/x/c/8b88b81c064543d3/IQAq1c_k3axXRLWxLt8SQu3BARRlXjhe-6rHVYqaGUqoWXQ?e=ic1tLV",
     2017: "https://1drv.ms/x/c/8b88b81c064543d3/IQDc3zffIXaSRpGrWZ01szLrAY_uynjSzFZBwOn8TmRoBgw?e=OHUat0",
     2018: "https://1drv.ms/x/c/8b88b81c064543d3/IQBS7Tn8RT-wSphRRctnk360AX9-hRShvBHFNumXwT_v-ms?e=hOmMM7",
-    2019: "",
-    2020: "",
-    2021: "",
+    2019: "https://1drv.ms/x/c/8b88b81c064543d3/IQAcbjTDEi04T4wVyRXUpD23AUinaaHENIyzFPFzGTid61Q?e=jz4roh",
+    2020: "https://1drv.ms/x/c/8b88b81c064543d3/IQDY095rujPVT7AYeGFZRS0nASKL043IU_XNnPiXfP3rPX0?e=IQMbqu",
+    2021: "https://1drv.ms/x/c/8b88b81c064543d3/IQDSHMP3TVnPQJ31tZI6-cjbAf9G3uglIzVlcLBOR7U-rok?e=aMc8UW",
     2022: "https://1drv.ms/x/c/8b88b81c064543d3/IQB7ufC0vxdtTbx2IWljufoMAXn9BT07BTaml3pvDIw59Zw?e=SwZndk",
     2023: "https://1drv.ms/x/c/8b88b81c064543d3/IQASP70qtOUER43nSlVhRnyBAfwNm9Ftc3BMNi9akvMfJ4M?e=gxf0iQ",
     2024: "https://1drv.ms/x/c/8b88b81c064543d3/IQBRY9FjSoiYTbqQBlaf7ow9ARAhVOTqupUR70uECefpMs8?e=crUVdI",
@@ -34,3 +34,4 @@ def load_urls():
 
 def get_url_by_year(urls_dict, year: int):
     return urls_dict.get(int(year))
+
