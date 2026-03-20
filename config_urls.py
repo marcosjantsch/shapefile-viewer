e7471397f903f26811d95dd25d37d69b9888ee20
marcosjantsch <85318367+marcosjantsch@users.noreply.github.com>
2026-03-20 10:08:05 -0300

    Update config_urls.py

--- config_urls.py
+++ config_urls.py
@@ -26,7 +26,7 @@ CSV_URLS = {
     2023: "https://1drv.ms/x/c/8b88b81c064543d3/IQASP70qtOUER43nSlVhRnyBAfwNm9Ftc3BMNi9akvMfJ4M?e=gxf0iQ",
     2024: "https://1drv.ms/x/c/8b88b81c064543d3/IQBRY9FjSoiYTbqQBlaf7ow9ARAhVOTqupUR70uECefpMs8?e=crUVdI",
     2025: "https://1drv.ms/x/c/8b88b81c064543d3/IQDEpcmoHOHLSYwHj8uo99hqAVYedbu5TwWoMlLZX1MwYNI?e=mNJvcU",
-    2026: "https://1drv.ms/x/c/8b88b81c064543d3/IQDp1oP66PDrTZBXYkKw8zcfAcVsQU3GmoZwqZsvrW1oFcI?e=LZIvnm",
+    2026: "https://1drv.ms/x/c/8b88b81c064543d3/IQDp1oP66PDrTZBXYkKw8zcfAcVsQU3GmoZwqZsvrW1oFcI?e=iHYaXd",
 		
     }
 
